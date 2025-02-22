ce87252f2d5415448dd0761fb86e9eaf8ef21f9f
sozge <sozgeozkartal@gmail.com>
2025-02-22 17:21:33 +0300

    ZodiacPage designed

--- app/src/main/java/com/sozge/rani/pages/mainpages/main/ZodiacPage.kt
+++ app/src/main/java/com/sozge/rani/pages/mainpages/main/ZodiacPage.kt
@@ -1,161 +1,168 @@
 package com.sozge.rani.pages.mainpages.main
 
+
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.PagerState
-import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.sozge.rani.R
+import com.sozge.rani.components.CustomText
 import com.sozge.rani.components.FloatingBottomNavBar
 import com.sozge.rani.components.HeaderBar
 
 @Composable
 fun ZodiacPage(navController: NavController) {
-    var selectedTab by remember { mutableStateOf("Burçlar") }
-    val burclar = listOf(
-        "Koç",
-        "Boğa",
-        "İkizler",
-        "Yengeç",
-        "Aslan",
-        "Başak",
-        "Terazi",
-        "Akrep",
-        "Yay",
-        "Oğlak",
-        "Kova",
-        "Balık"
-    )
-    val pagerState = rememberPagerState(pageCount = { burclar.size }, initialPage = 2)
+    val horoscopes = listOf("Koç", "Boğa", "İkizler", "Yengeç", "Aslan", "Başak", "Terazi", "Akrep", "Yay", "Oğlak", "Kova", "Balık")
+    var selectedHoroscope by remember { mutableStateOf(1) }
+    val scrollState = rememberScrollState()
+    val categories = listOf("Genel Özellikler", "Günlük Burç", "Takım Yıldızları")
+    val cardScrollState = rememberScrollState()
+    var selectedCardIndex by remember { mutableStateOf(1) }
+
+    LaunchedEffect(cardScrollState.value) {
+        val index = (cardScrollState.value / 220).coerceIn(0, categories.size - 1)
+        selectedCardIndex = index
+    }
+    LaunchedEffect(scrollState.value) {
+        val index = (scrollState.value / 100).coerceIn(0, horoscopes.size - 1)
+        selectedHoroscope = index
+    }
 
     Scaffold(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.White),
+            .background(Color.Black),
         topBar = {
             HeaderBar(
                 title = "Burçlar",
                 isEnableBackButton = false,
                 navController = navController
             )
-        },
-        bottomBar = {
-            FloatingBottomNavBar(
-                selectedTab = selectedTab,
-                onTabSelected = { selectedTab = it },
-                navController
-            )
         }
     ) { innerPadding ->
         Column(
             modifier = Modifier
+                .padding(innerPadding)
                 .fillMaxSize()
-                .padding(innerPadding),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(16.dp)
+                .background(MaterialTheme.colorScheme.background),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            BurcSelector(pagerState, burclar)
-            BurcCardPager(pagerState.currentPage)
-        }
-    }
-}
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .horizontalScroll(scrollState),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                horoscopes.forEachIndexed { index, name ->
+                    val scale by animateFloatAsState(if (index == selectedHoroscope) 1.4f else 0.7f)
+                    val zodiacIcon = when (name) {
+                        "Koç" -> painterResource(id = R.drawable.aries)
+                        "Boğa" -> painterResource(id = R.drawable.taurus)
+                        "İkizler" -> painterResource(id = R.drawable.gemini)
+                        "Yengeç" -> painterResource(id = R.drawable.cancer)
+                        "Aslan" -> painterResource(id = R.drawable.leo)
+                        "Başak" -> painterResource(id = R.drawable.virgo)
+                        "Terazi" -> painterResource(id = R.drawable.libra)
+                        "Akrep" -> painterResource(id = R.drawable.scorpio)
+                        "Yay" -> painterResource(id = R.drawable.sagittarius)
+                        "Oğlak" -> painterResource(id = R.drawable.capricorn)
+                        "Kova" -> painterResource(id = R.drawable.aquarius)
+                        "Balık" -> painterResource(id = R.drawable.pisces)
+                        else -> painterResource(id = R.drawable.categorybackground)
+                    }
 
-@Composable
-fun BurcSelector(pagerState: PagerState, burclar: List<String>) {
-    HorizontalPager(state = pagerState, modifier = Modifier.height(120.dp)) { page ->
-        val isSelected = pagerState.currentPage == page
-        val scale by animateFloatAsState(if (isSelected) 1.4f else 0.9f, label = "Scale")
-        val alpha by animateFloatAsState(if (isSelected) 1f else 0.5f, label = "Alpha")
-
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(100.dp)
-                .scale(scale)
-                .alpha(alpha),
-            contentAlignment = Alignment.Center
-        ) {
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Box(
-                    modifier = Modifier
-                        .size(60.dp)
-                        .background(
-                            if (isSelected) Color.White else Color.Gray,
-                            shape = RoundedCornerShape(30.dp)
-                        )
-                )
-                Text(
-                    text = burclar[page],
-                    fontSize = 18.sp,
-                    color = if (isSelected) Color.White else Color.Gray
-                )
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier.padding(horizontal = 10.dp)
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .size(96.dp)
+                                .scale(scale)
+                                .background(
+                                    if (index == selectedHoroscope) MaterialTheme.colorScheme.primary
+                                    else MaterialTheme.colorScheme.background,
+                                    shape = CircleShape
+                                )
+                                .padding(8.dp),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Icon(
+                                painter = zodiacIcon,
+                                contentDescription = "$name burcu",
+                                tint = Color.Unspecified,
+                                modifier = Modifier.fillMaxSize()
+                            )
+                        }
+                        CustomText(name, fontSize = 20.sp, color = Color.White)
+                    }
+                }
             }
-        }
-    }
-}
 
-@Composable
-fun BurcCardPager(selectedBurcIndex: Int) {
-    val cardTitles = listOf("Genel Özellikler", "Günlük Yorum", "Yıldız Haritası")
-    val pagerState = rememberPagerState(pageCount = { cardTitles.size }, initialPage = 1)
 
-    HorizontalPager(state = pagerState, modifier = Modifier.fillMaxHeight()) { page ->
-        val isSelected = pagerState.currentPage == page
-        val scale by animateFloatAsState(if (isSelected) 1f else 0.85f, label = "Scale")
-        val alpha by animateFloatAsState(if (isSelected) 1f else 0.6f, label = "Alpha")
-
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(800.dp)
-                .scale(scale)
-                .alpha(alpha)
-                .clip(RoundedCornerShape(20.dp))
-                .background(Color.Blue.copy(alpha = 0.5f))
-                .padding(16.dp),
-            contentAlignment = Alignment.BottomCenter
-        ) {
             Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, end = 16.dp),
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
-                Text(text = cardTitles[page], fontSize = 22.sp, color = Color.White)
-                Text(
-                    text = "Burcun özel bilgileri burada yer alacak...",
-                    fontSize = 16.sp,
-                    color = Color.White
-                )
-                Button(onClick = { /* Detay sayfasına git */ }) {
-                    Text("Detaylıca Oku")
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .horizontalScroll(cardScrollState)
+                ) {
+                    categories.forEachIndexed { index, category ->
+                        val scale by animateFloatAsState(if (index == selectedCardIndex) 1f else 0.9f)
+                        Card(
+                            shape = RoundedCornerShape(20.dp),
+                            modifier = Modifier
+                                .width(340.dp)
+                                .height(520.dp)
+                                .padding(8.dp)
+                        ) {
+                            Column(
+                                modifier = Modifier.padding(16.dp),
+                                verticalArrangement = Arrangement.SpaceBetween
+                            ) {
+                                Text(category, fontSize = 16.sp, fontWeight = FontWeight.Bold)
+                                Button(onClick = { navController.navigate("ZodiacDailyCommentPage")}) {
+                                    Text("Devamını Oku")
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
     }
-
 }
+
+
+
